0764407a7bc4217b4466e2c66f0068dfdaf418b4
xiaojixing <240040543@qq.com>
2018-05-20 13:12:44 +0800

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -18,11 +18,19 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
-
 
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -39,10 +39,9 @@
                 @else
                 <li class="dropdown">
                     <a aria-expanded="false" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-                        <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                            <img class="img-responsive img-circle" height="30px" src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" width="30px">
-                            </img>
-                        </span>
+                         <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+                         </span>
                         {{ Auth::user()->name }}
                         <span class="caret">
                         </span>
