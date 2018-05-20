ccb8063c5c51e75c7980cac9045d0078285f079c
xiaojixing <240040543@qq.com>
2018-05-20 14:49:46 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,9 @@ class User extends Authenticatable
     }
 
     
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+    
 }
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -2,18 +2,18 @@
 
 namespace App\Policies;
 
-use App\Models\Topic;
 use App\Models\User;
+use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $user->isAuthorOf($topic);
     }
 }
