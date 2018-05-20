36c1214a40123344f13989cc636de4bd9b2cea40
xiaojixing <240040543@qq.com>
2018-05-20 18:06:22 +0800

    User can delete reply

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -28,4 +28,12 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
+
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
+
+    
 }
