5854c9364915e0aea5135340e4763c2f50e1ce67
xiaojixing <240040543@qq.com>
2018-05-20 17:56:54 +0800

    Notify author when topic replied

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'PagesController@root')->name('root');
 
@@ -25,3 +25,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
