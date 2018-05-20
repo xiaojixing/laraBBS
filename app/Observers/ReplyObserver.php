977220717de7b763ff422c9842d1eafaea260763
xiaojixing <240040543@qq.com>
2018-05-20 17:33:18 +0800

    User can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,9 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
-    }
-
-    public function updating(Reply $reply)
-    {
-        //
+        $reply->topic->increment('reply_count', 1);
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
